cce16d59d52c0e8a38a03223d19ac5b60eb2b3f4
teddythinh <cuongthinh2002@gmail.com>
2023-09-10 19:12:56 +0700

    First Initialize

---
+++ index.ts
@@ -0,0 +1,11 @@
+import figlet from "figlet";
+const server = Bun.serve({
+    fetch() {
+      const body = figlet.textSync("Bun!");
+      return new Response(body);
+    },
+    port: 3000,
+  });
+  
+  console.log(`Listening on http://localhost:${server.port} ...`);
+  
